8e4069b52909182f441503f95a96a9b387d05334
Brianm0823 <49293194+Brianm0823@users.noreply.github.com>
2019-12-02 20:21:53 -0600

    Final changes to Admin page
    Regular admins can no longer make other accounts into superadmins. Only superadmins can edit that user field

--- admin_portal_project/users/admin.py
+++ admin_portal_project/users/admin.py
@@ -4,6 +4,8 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Permission
 from django.contrib import admin
 from django.contrib.sites.models import Site
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
@@ -14,6 +16,29 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['email', 'username',]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+
+class MyUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+
+        if request.user.is_superuser:
+            perm_fields = ('is_active', 'is_staff', 'is_superuser',
+                           'groups', 'user_permissions')
+        else:
+            # modify these to suit the fields you want your
+            # staff user to be able to edit
+            perm_fields = ('is_active', 'is_staff', 'groups')
+
+        return [(None, {'fields': ('username', 'password')}),
+                (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+                (_('Permissions'), {'fields': perm_fields}),
+                (_('Important dates'), {'fields': ('last_login', 'date_joined')})]
+
+admin.site.register(CustomUser, MyUserAdmin)
 admin.site.register(Permission)
 admin.site.unregister(Site)
